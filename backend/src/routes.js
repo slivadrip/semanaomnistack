2da486393cfc25a67414e6723bc78dc55c14db96
Adriano <slivadrip@gmail.com>
2020-01-15 15:50:44 -0300

    update backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,12 @@
 const { Router } = require('express');
-const axios =  require('axios');
+const DevController = require('./controllers/DevController')
+const SearchController = require('./controllers/SearchController')
 
 const routes = Router();
 
-routes.post('/devs', async (request, response) => {
-    const{ github_username } =  request.body;
+routes.get('/devs', DevController.index)
+routes.post('/devs', DevController.store);
 
-const response = axios.get(`https://api.github.com/users/${github_username}`);
-
-    return response.json({message: 'Hello World 2'});
-});
+routes.get('/search', SearchController.index)
 
 module.exports = routes;
